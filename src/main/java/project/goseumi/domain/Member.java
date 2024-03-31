f47c579f9859646d60c51ddd8504a734a090f728
KwangHo <skh990427@naver.com>
2024-03-31 23:02:25 +0900

    테이블 생성 및 Q객체 생성 확인

---
+++ src/main/java/project/goseumi/domain/Member.java
@@ -0,0 +1,21 @@
+package project.goseumi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+}
