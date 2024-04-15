42068212722f8679c1bea421944eb18ad09db2a5
KwangHo <skh990427@naver.com>
2024-04-16 00:03:47 +0900

    feat: Sessions 세션코드 필드 추가

--- src/main/java/project/goseumi/domain/Sessions.java
+++ src/main/java/project/goseumi/domain/Sessions.java
@@ -6,6 +6,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Entity
 @Getter
 public class Sessions extends BaseEntity {
@@ -13,4 +15,10 @@ public class Sessions extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    private String sessionsCode;
+
+    protected Sessions() {
+        sessionsCode = "session_" + UUID.randomUUID().toString().replace("-", "");
+    }
 }
