f6449c6c75dde7b9a7afcd620557acdc144a7653
KwangHo <skh990427@naver.com>
2024-04-17 23:37:43 +0900

    feat: LoginResponse 추가

--- src/main/java/project/goseumi/controller/dto/response/LoginResponse.java
+++ src/main/java/project/goseumi/controller/dto/response/LoginResponse.java
@@ -4,4 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class LoginResponse {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    public LoginResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
