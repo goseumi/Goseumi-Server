34e41ddc0b1cc98ae9f2241b895257dda34d6c6a
KwangHo <skh990427@naver.com>
2024-04-28 23:12:47 +0900

    jwt UserDetailService 구현

---
+++ src/main/java/project/goseumi/security/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package project.goseumi.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import project.goseumi.exception.BusinessException;
+import project.goseumi.exception.error.MemberError;
+import project.goseumi.repository.MemberRepository;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public UserDetailServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .orElseThrow(() -> new BusinessException(MemberError.USER_LOGIN_FAIL));
+    }
+}
