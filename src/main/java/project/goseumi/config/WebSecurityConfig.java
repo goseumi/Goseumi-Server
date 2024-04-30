bba97776005753f01dfca35e47fa75fea3e16b9e
KwangHo <skh990427@naver.com>
2024-04-30 22:50:02 +0900

    시큐리티 jwt 필터 등록

--- src/main/java/project/goseumi/config/WebSecurityConfig.java
+++ src/main/java/project/goseumi/config/WebSecurityConfig.java
@@ -2,16 +2,31 @@ package project.goseumi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import project.goseumi.security.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public WebSecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -27,6 +42,9 @@ public class WebSecurityConfig {
                 //api 접근권한 설정
                 .authorizeHttpRequests((auth) -> auth
                         .anyRequest().permitAll())
+
+                //jwt 필터 추가
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class)
 
                 //jwt 세션 stateless
                 .sessionManagement((session) -> session
