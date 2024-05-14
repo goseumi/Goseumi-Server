944611a970ef17929c78c29c76015c15f1959fe4
KwangHo <skh990427@naver.com>
2024-05-14 15:58:21 +0900

    feat :: BoardCategory 등록 기능 구현

--- src/main/java/project/goseumi/service/BoardCategoryService.java
+++ src/main/java/project/goseumi/service/BoardCategoryService.java
@@ -3,6 +3,8 @@ package project.goseumi.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import project.goseumi.controller.dto.request.CreateCategoryRequestDto;
+import project.goseumi.domain.BoardCategory;
 import project.goseumi.repository.BoardCategoryRepository;
 
 @Service
@@ -11,5 +13,11 @@ import project.goseumi.repository.BoardCategoryRepository;
 public class BoardCategoryService {
 
     private final BoardCategoryRepository boardCategoryRepository;
+
+    public String createCategory(CreateCategoryRequestDto createCategoryRequestDto) {
+        BoardCategory newCategory = BoardCategory.of(createCategoryRequestDto);
+        boardCategoryRepository.save(newCategory);
+        return newCategory.getName();
+    }
 
 }
