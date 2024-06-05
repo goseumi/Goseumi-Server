4e0c4d8206b0d60098a93dcd0baa42d386e7bc80
KwangHo <skh990427@naver.com>
2024-06-05 17:41:26 +0900

    feat :: 학교 인증 등록 api 구현

--- src/main/java/project/goseumi/service/MemberService.java
+++ src/main/java/project/goseumi/service/MemberService.java
@@ -1,16 +1,24 @@
 package project.goseumi.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.goseumi.controller.dto.request.LoginRequest;
+import project.goseumi.controller.dto.request.SchoolAuthRequest;
 import project.goseumi.controller.dto.request.SignUpRequest;
 import project.goseumi.controller.dto.response.LoginResponse;
 import project.goseumi.domain.Member;
+import project.goseumi.domain.School;
+import project.goseumi.domain.SchoolAuth;
 import project.goseumi.exception.BusinessException;
 import project.goseumi.exception.error.MemberError;
+import project.goseumi.exception.error.SchoolError;
 import project.goseumi.repository.MemberRepository;
+import project.goseumi.repository.SchoolAuthRepository;
+import project.goseumi.repository.SchoolRepository;
+import project.goseumi.security.jwt.JwtUtil;
 
 import java.util.Optional;
 
@@ -20,7 +28,10 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final SchoolAuthRepository schoolAuthRepository;
+    private final SchoolRepository schoolRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public Long createNewAccount(final SignUpRequest signUpRequest) {
@@ -80,5 +91,45 @@ public class MemberService {
         if (!passwordEncoder.matches(inputPassword, expectedPassword)) {
             throw new BusinessException(MemberError.USER_LOGIN_FAIL);
         }
+    }
+
+    @Transactional
+    public Long newSchoolAuth(HttpServletRequest request, SchoolAuthRequest schoolAuthRequest) {
+        String userEmail = getUserEmailFromToken(request);
+
+        Member member = memberRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new BusinessException(MemberError.USER_LOGIN_FAIL));
+
+        School school = schoolRepository.findById(schoolAuthRequest.getSchoolId())
+                .orElseThrow(() -> new BusinessException(SchoolError.SCHOOL_FIND_BY_ID_FAIL));
+
+        SchoolAuth schoolAuth = SchoolAuth.of(member, school, schoolAuthRequest.getImgUrl());
+
+        schoolAuthRepository.save(schoolAuth);
+
+        return schoolAuth.getId();
+    }
+
+    /**
+     * 토큰에서 username(email) 뽑아오기
+     */
+    private String getUserEmailFromToken(HttpServletRequest request) {
+        String pureToken = getPureToken(request);
+
+        return jwtUtil.getUsername(pureToken);
+    }
+
+    /**
+     * Authorization 헤더에서 Bearer 제거하고 순수 토큰 반환
+     */
+    private String getPureToken(HttpServletRequest request) {
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            throw new BusinessException(MemberError.NOT_EXIST_TOKEN);
+        }
+
+        return authorization.split(" ")[1];
     }
 }
